9caa6df243259fa65dbd1e188c9c4e85b9832fa0
WinterJackson <winterjacksonwj@gmail.com>
2023-08-31 10:14:28 +0300

    Completed Assignment

--- lib/testing/module_test.py
+++ lib/testing/module_test.py
@@ -1,3 +1,8 @@
+from lib.absolute_package.package1 import module1
+from lib.absolute_package.package1.module1 import function1
+from lib.absolute_package.package2.subpackage1.module6 import function1
+
+
 class TestModule:
     '''placeholder test'''
 
